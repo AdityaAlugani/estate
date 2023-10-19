89c5f641dbb63cc990f516f243bb871e09b99b60
Aditya Alugani <marvelsuniverse11967@gmail.com>
2023-10-20 01:15:05 +0530

    Created a test api route

--- OneDrive/Desktop/New folder(3)/api/index.js
+++ OneDrive/Desktop/New folder(3)/api/index.js
@@ -1,14 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config()
 const app=express();
+import userRouter from "./routes/user.route.js";
 
 mongoose.connect("mongodb+srv://marvelsuniverse1967:Ngit%40123%24@mern-estate.02f7f0i.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>{
     console.log("Connected to mongodb server");
 }).catch((error)=>console.log(error.message));
 
-app.listen(3001,()=>{
-    console.log("Server is running at port 3001!");
+app.listen(4001,()=>{
+    console.log("Server is running at port 4001!");
 });
+
+app.use('/',userRouter);
