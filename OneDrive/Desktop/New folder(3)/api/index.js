18d5abed074eec90b7197e3c594dc35f6ba98e46
Aditya Alugani <marvelsuniverse11967@gmail.com>
2023-10-20 01:59:27 +0530

    Create signup api route

--- OneDrive/Desktop/New folder(3)/api/index.js
+++ OneDrive/Desktop/New folder(3)/api/index.js
@@ -2,14 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 const app=express();
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 mongoose.connect("mongodb+srv://marvelsuniverse1967:Ngit%40123%24@mern-estate.02f7f0i.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>{
     console.log("Connected to mongodb server");
 }).catch((error)=>console.log(error.message));
 
+app.use(express.json());
+
 app.listen(4001,()=>{
     console.log("Server is running at port 4001!");
 });
 
-app.use('/',userRouter);
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
--- OneDrive/Desktop/New folder(3)/api/models/user.model.js
+++ OneDrive/Desktop/New folder(3)/api/models/user.model.js
@@ -5,7 +5,7 @@ const userSchema=new mongoose.Schema({
         required:true,
         unique:true,
     },
-    email:{
+    gmail:{
         type:String,
         required:true,
         unique:true,
