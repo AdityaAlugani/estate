4eda08e6efd04415c23b87dbe12c4e92502f862b
Aditya Alugani <marvelsuniverse11967@gmail.com>
2023-10-23 23:52:26 +0530

    Add redux-persist

--- OneDrive/Desktop/New folder(3)/client/src/redux/store.js
+++ OneDrive/Desktop/New folder(3)/client/src/redux/store.js
@@ -1,11 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice.js'
+import storage from 'redux-persist/lib/storage';
+import { persistStore } from 'redux-persist';
+import {persistReducer} from 'redux-persist';
+
+const rootReducer=combineReducers({
+    user:userReducer,
+})
+
+const persistConfig={
+    key:'root',
+    storage,
+    version:1,
+}
+
+const persistedReducer=persistReducer(persistConfig,rootReducer);
+
 
 export const store = configureStore({
-  reducer: {user:userReducer},
+  reducer: {user:persistedReducer},
   middleware:(getDefaultMiddleware)=>{
     return getDefaultMiddleware({
         serializableCheck:false,
     })
   }
-})
+});
+
+export const persistor=persistStore(store);
