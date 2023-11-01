7bc9c3bd3954c34e69a44693484d597ff0d95d7f
Aditya Alugani <marvelsuniverse11967@gmail.com>
2023-11-02 00:36:00 +0530

    Complete show user listings functions

--- OneDrive/Desktop/New folder(3)/api/routes/user.route.js
+++ OneDrive/Desktop/New folder(3)/api/routes/user.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { deleteUser, test, updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/VerifyUser.js";
+import { getUserListings } from "../controllers/listing.controller.js";
 
 const router=express.Router();
 
 router.get('/test',test);
 router.post('/update/:id',verifyToken,updateUser);
 router.delete('/delete/:id',verifyToken,deleteUser);
+router.get('/listings/:userid',verifyToken,getUserListings);
 export default router;
--- OneDrive/Desktop/New folder(3)/client/src/pages/Profile.jsx
+++ OneDrive/Desktop/New folder(3)/client/src/pages/Profile.jsx
@@ -10,7 +10,9 @@ import { signOut } from "firebase/auth";
 const Profile=()=>{
     const currentUser=useSelector((state)=>state.user.user.currentUser);
     const Loading=useSelector((state)=>state.user.user.loading);
+    const [showListingsError,setShowListingsError]=useState(false);
     const Error=useSelector((state)=>state.user.user.error);
+    const [userListings,setUserListings]=useState(null);
     const fileRef=useRef(null);
     const dispatch=useDispatch();
     const [file,setFile]=useState(undefined);
@@ -19,7 +21,7 @@ const Profile=()=>{
     const [formData,setFormData]=useState({});
     const navigate=useNavigate();
     //console.log(filePercentage+"%");
-    console.log(formData);
+    //console.log(formData);
     //console.log(currentUser.gmail);
 
     const handleFileSubmit=(file)=>{
@@ -55,7 +57,7 @@ const Profile=()=>{
     };
     const handleSubmit=async (e)=>{
         //setFormData({...currentUser,...formData});
-        console.log("Form data before sending",formData);
+        //console.log("Form data before sending",formData);
         e.preventDefault();
         try{
             dispatch(updateUserStart());
@@ -123,6 +125,28 @@ const Profile=()=>{
         }
         
     }
+    const handleShowListings=async ()=>{
+        console.log("CHANGE");
+        try{
+            setShowListingsError(false);
+            const res=await fetch(`api/user/listings/${currentUser._id}`,{
+                method:'GET',
+            });
+            const data=await res.json();
+            if(data.success===false)
+            {
+                //console.log("Error in the data.success");
+                setShowListingsError(true);
+                return;
+            }
+            setShowListingsError(false);
+            setUserListings(data);
+        }
+        catch(error)
+        {
+            setShowListingsError(true);
+        }
+    }
     return <div className="max-w-lg mx-auto p-4">
         <h1 className="text-3xl p-3 m-3 font-semibold text-center">Profile</h1>
         <input onChange={(e)=>setFile(e.target.files[0])} hidden accept="image/*" type="file" ref={fileRef} />
@@ -132,14 +156,39 @@ const Profile=()=>{
             <input onChange={handleFormChange} placeholder="gmail" defaultValue={currentUser.gmail} className="rounded-xl p-3" id="gmail" />
             <input onChange={handleFormChange} placeholder="username" defaultValue={currentUser.username} className="rounded-xl p-3" id="username" />
             <input onChange={handleFormChange} placeholder="password" defaultValue={currentUser.password} className="rounded-xl p-3" id="password" />
-            <button disabled={Loading} className="bg-wbrown text-googlewhite p-3 rounded-xl">{!Loading ? "Update" : "Loading..."}</button>
-            <button onClick={navigate('/create-listing')} className="bg-wblue text-googlewhite p-3 rounded-xl">Create Listings</button>
+            <button onClick={()=>handleSubmit()} disabled={Loading} className="bg-wbrown text-googlewhite p-3 rounded-xl">{!Loading ? "Update" : "Loading..."}</button>
+            <button onClick={()=>navigate('/create-listing')} className="bg-wblue text-googlewhite p-3 my-2 rounded-xl">Create Listings</button>
         </form>
+
         <div className="flex justify-between pt-2 mt-2">
+            
             <p onClick={handleDelete} className="text-brownLight hover:text-red cursor-pointer">Delete account?</p>
             <p onClick={handleSignOut}className="text-brownLight hover:text-red cursor-pointer">Sign out</p>
         </div>
         <p className="text-center">{Error ? <span className="text-brown">{Error}</span> : <span className="text-green">Updated Successfully!</span>}</p>
+        <p className="text-redLight font-semibold">{showListingsError ? "Error showing listings" : ""}</p>
+        <p onClick={()=>handleShowListings()} className="text-wblue text-center text-googlewhite p-3 my-2 rounded-xl">Show Listings</p>
+
+        {
+            userListings && userListings.length>0 &&
+            <div className="flex flex-col"> 
+            <h2 className="text-center font-semibold text-2xl mb-5 underline">Your-Listings</h2>
+            {userListings.map((listing)=>{
+                            return <div key={listing._id} className="flex flex-row justify-between border border-pinkLight  p-1 rounded-md my-2">
+                                <img onClick={()=>navigate(`/listings/${listing._id}`)} src={listing.imageUrls[0]}  className="w-24 h-16 object-contain self-center" />
+                                <p onClick={()=>navigate(`/listings/${listing._id}`)} className="self-center font-light cursor-pointer uppercase hover:underline  text-green">{listing.name}</p>
+                                <div className="flex flex-col self-center">
+                                    <button className="p-1 text-red hover:underline">DELETE</button>
+                                    <button className="text-googleblue hover:underline">EDIT</button>
+                                </div>
+
+                            </div>
+                        })}
+
+            </div>
+            
+        }
+
     </div>
 }
 
